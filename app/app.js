a742d5fe0735c3e213c23f507e3c73a7255f57ca
Hazel Seanor <avellana1111@gmail.com>
2018-03-22 23:37:06 +0000

    moved API to API path

--- app/app.js
+++ app/app.js
@@ -16,8 +16,9 @@ app.use(express.static('public'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+/* REST API */
 
-app.get("/assignments/", function (req, res) { /* add search by due date functionality */
+app.get("api/assignments/", function (req, res) { /* add search by due date functionality */
     let valid_params = ['name'];
     let filter_params = {};
 
@@ -34,11 +35,7 @@ app.get("/assignments/", function (req, res) { /* add search by due date functio
         });
 });
 
-app.get("/", function (req, res) {
-    res.render('index', {showGroups: true, group : [{title: 'Group', description: 'A group'},{title: 'Group 2', description: 'A group'}]});
-});
-
-app.get("/assignments/:assignment_id", function (req, res) {
+app.get("api/assignments/:assignment_id", function (req, res) {
     if (validator.isInt(req.params['assignment_id'])) {
         dbConnector.searchAssignments({'assignment_id': req.params.assignment_id})
             .then(function (results) {
@@ -51,7 +48,7 @@ app.get("/assignments/:assignment_id", function (req, res) {
     }
 });
 
-app.get("/assignments/:assignment_id/submissions", function (req, res) {
+app.get("api/assignments/:assignment_id/submissions", function (req, res) {
     if (validator.isInt(req.params['assignment_id'])) {
         dbConnector.findAssignmentSubmissions(req.params['assignment_id'], 0, 5)
             .then(function (results) {
@@ -65,7 +62,7 @@ app.get("/assignments/:assignment_id/submissions", function (req, res) {
 });
 
 
-app.get("/users/", function (req, res) {
+app.get("api/users/", function (req, res) {
     let valid_params = ['name', 'role_id', 'birthday', 'gender', 'email'];
     let filter_params = {};
 
@@ -82,7 +79,7 @@ app.get("/users/", function (req, res) {
         });
 });
 
-app.get("/users/:user_id", function (req, res) {
+app.get("api/users/:user_id", function (req, res) {
     if (validator.isInt(req.params['user_id'])) {
         dbConnector.searchUsers({'user_id': req.params.user_id})
             .then(function (results) {
@@ -95,7 +92,7 @@ app.get("/users/:user_id", function (req, res) {
     }
 });
 
-app.get("/users/:user_id/groups", function (req, res) {
+app.get("api/users/:user_id/groups", function (req, res) {
     if (validator.isInt(req.params['user_id'])) {
         dbConnector.findUserGroups(req.params['user_id'], 0, 5)
             .then(function (results) {
@@ -108,7 +105,7 @@ app.get("/users/:user_id/groups", function (req, res) {
     }
 });
 
-app.get("/users/:user_id/submissions", function (req, res) {
+app.get("api/users/:user_id/submissions", function (req, res) {
     if (validator.isInt(req.params['user_id'])) {
         dbConnector.findUserSubmissions(req.params['user_id'], 0, 5)
             .then(function (results) {
@@ -121,7 +118,7 @@ app.get("/users/:user_id/submissions", function (req, res) {
     }
 });
 
-app.get("/groups/", function (req, res) {
+app.get("api/groups/", function (req, res) {
     let valid_params = ['name'];
     let filter_params = {};
 
@@ -138,7 +135,7 @@ app.get("/groups/", function (req, res) {
         });
 });
 
-app.get("/groups/:group_id", function (req, res) {
+app.get("api/groups/:group_id", function (req, res) {
     if (validator.isInt(req.params['group_id'])) {
         dbConnector.searchGroups({'group_id': req.params.group_id})
             .then(function (results) {
@@ -151,7 +148,7 @@ app.get("/groups/:group_id", function (req, res) {
     }
 });
 
-app.get("/groups/:group_id/users", function (req, res) {
+app.get("api/groups/:group_id/users", function (req, res) {
     if (validator.isInt(req.params['group_id'])) {
         dbConnector.findGroupUsers(req.params['group_id'], 0, 5)
             .then(function (results) {
@@ -164,7 +161,7 @@ app.get("/groups/:group_id/users", function (req, res) {
     }
 });
 
-app.get("/groups/:group_id/users", function (req, res) {
+app.get("api/groups/:group_id/users", function (req, res) {
     if (validator.isInt(req.params['group_id'])) {
         dbConnector.findGroupUsers(req.params['group_id'], 0, 5)
             .then(function (results) {
@@ -177,7 +174,7 @@ app.get("/groups/:group_id/users", function (req, res) {
     }
 });
 
-app.get("/groups/:group_id/assignments", function (req, res) {
+app.get("api/groups/:group_id/assignments", function (req, res) {
     if (validator.isInt(req.params['group_id'])) {
         dbConnector.findGroupAssignments(req.params['group_id'], 0, 5)
             .then(function (results) {
